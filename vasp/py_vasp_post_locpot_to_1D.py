df27fa163dd85993169c716020ebc5886fd1e4b6
Ji-Hwan, Lee <jkwnlee88@gmail.com>
2018-07-06 17:10:57 +0900

    Update py_vasp_post_locpot_to_1D.py

--- vasp/py_vasp_post_locpot_to_1D.py
+++ vasp/py_vasp_post_locpot_to_1D.py
@@ -11,61 +11,8 @@
 
 import os
 import math 
-
-def r_cryst_vasp(filename):
-    """
-    Filename: VASP, POSCAR TYPE
-    Function: Read POSCAR type 
-    """
-    poscar=[]; unitcell=[]; compound=[]; position=[];scales=[];num_atoms=0
-    with open(filename, 'r') as f:
-        i = 1; j = 1; k =0
-        for line in f:
-            if line == None: break
-            if i > 2 and i <6:
-                unitcell.append(line.split())
-            elif i > 5 and i < 8:
-                compound.append(line.split())
-            elif i == 8 :
-                if j == 1 :
-                    if line.split()[0][0] == 'S':
-                        # print 'Selective Dynamics are applied'
-                        Selective= True
-                        i = i - 1
-                    j = 2
-                if j == 2:
-                    if line.split()[0][0] == 'C':
-                        scales=[1.,1.,1.]
-                    elif line.split()[0][0] == 'D':
-                        scales=[float(unitcell[0][0]) + float(unitcell[1][0]) + float(unitcell[2][0]),\
-                                float(unitcell[0][1]) + float(unitcell[1][1]) + float(unitcell[2][1]),\
-                                float(unitcell[0][2]) + float(unitcell[1][2]) + float(unitcell[2][2])]          
-                if num_atoms == 0:
-                    for temp in compound[1]: num_atoms=num_atoms+int(temp)
-
-            elif i > 8 :
-                if i <= 8 + num_atoms:
-                    x = scales[0]  * float(line.split()[0])
-                    y = scales[1]  * float(line.split()[1])
-                    z = scales[2]  * float(line.split()[2])
-                    if k < int(compound[1][0]):
-                        if Selective:
-                            position.append([compound[0][0], x, y, z, line.split()[3], line.split()[4], line.split()[5]])
-                        else:
-                            position.append([compound[0][0], x, y, z])
-                    elif int(compound[1][0]) <= k <= int(compound[1][1]):
-                        if Selective:
-                            position.append([compound[0][1], x, y, z, line.split()[3], line.split()[4], line.split()[5]])
-                        else:
-                            position.append([compound[0][1], x, y, z])
-                    k= k+1
-
-            if i == 8 + num_atoms:
-                return unitcell, compound, position
-            else:
-                i = i + 1
-    # poscar=[unitcell, compound, position]
-
+import sys
+import JH_lib as jh
 
 def read_LOCPOT_line(filename):
 	with open(filename, 'r') as f:
@@ -93,8 +40,7 @@ def read_LOCPOT_line(filename):
 	return line_density
 
 
-def write_LOCPOT_line(unitcell,local_potental,plot=False, output_name='output_lp.txt'):
-    result=[]
+def write_LOCPOT_line(unitcell, local_potental, plot=False,  output_name='output_lp.txt'):
     f=open(output_name,'w' )
     f.write('%22.16s%22.16s \n' %('distance','local_potental'))
     for a in range(len(local_potental)):
@@ -103,47 +49,74 @@ def write_LOCPOT_line(unitcell,local_potental,plot=False, output_name='output_lp
 
     if plot:
         import matplotlib.pyplot as plt
-
         f = open(output_name,'r')
         x=[]; y=[]; i = 1
         for a in f:
-            if i == 1: i = i + 1
+#            print a.split(),i,
+
+            if i == 1: pass 
             else:
-                x.append(a.split()[0])
-                y.append(a.split()[1])
+                x.append(float(a.split()[0]))
+                y.append(float(a.split()[1]))
+            i = i + 1 
+            
         plt.plot(x,y)
         plt.savefig(output_name.replace('txt', 'png'))
 
 
-if len(sys.argv) > 1:
-    poscar = './input/POSCAR'
-    locpot = './input/LOCPOT'
-    print '''\
-[CODE] Automatically choose the input file: POSCAR and CONTCAR like,\n
-[CODE] POSCAR : %s
-[CODE] LOCPOT : %s
-[CODE] 
-[CODE] if you want to choose certain file,
-[CODE] $ python  py_vasp_post_chgcar_to_1D.py  ./input/POSCAR ./input/LOCPOT ''' %(poscar, locpot)
-else:
-    poscar = sys.argv[1]
-    locpot = sys.argv[2]
-    print '''\
-[CODE] You choose the input file: POSCAR and LOCPOT like,\n
-[CODE] POSCAR : %s
-[CODE] LOCPOT : %s ''' %(poscar, locpot)
-                  
-
+def check_the_input_path(target):
+    if os.path.isfile(target) == False:
+        print('[ERROR]  There is no input files for structure: %s' %target)
+        print('[ERROR]  Stop the calculation')
+        sys.exit()
 
 def main():
-	unitcell, compound, position = r_cryst_vasp(poscar)
+    if len(sys.argv) == 1:
+        poscar = './input/POSCAR'
+        locpot = './input/LOCPOT'
+        ploting= False
+        print '''\
+        [CODE] Automatically choose the input file: POSCAR and LOCPOT:
+        [CODE]     POSCAR : %s
+        [CODE]     LOCPOT : %s
+        [CODE] 
+        [CODE] if you want to choose certain file,
+        [CODE] $ python  py_vasp_post_chgcar_to_1D.py  ./input/POSCAR ./input/LOCPOT ''' %(poscar, locpot)
+    elif len(sys.argv) == 3:
+        poscar = sys.argv[1]
+        locpot = sys.argv[2]
+        ploting= False
+        print '''\
+        [CODE] You choose the input file: POSCAR and LOCPOT:
+        [CODE]     POSCAR : %s
+        [CODE]     LOCPOT : %s 
+        [CODE] 
+        [CODE] if you want to draw local potentail as function of z-axis,
+        [CODE] $ python  py_vasp_post_chgcar_to_1D.py  ./input/POSCAR ./input/LOCPOT True''' %(poscar, locpot)
+    elif len(sys.argv) == 4:
+        poscar = sys.argv[1]
+        locpot = sys.argv[2]
+        ploting= sys.argv[3]
+        print '''\
+        [CODE] You choose the input file: POSCAR and LOCPOT:
+        [CODE]     POSCAR : %s
+        [CODE]     LOCPOT : %s 
+        [CODE] 
+        [CODE] You allow the drawing LOCPOT (taking a time a bit)''' %(poscar, locpot)
 
-	total = read_LOCPOT_line(locpot)
-	write_LOCPOT_line(  unitcell        =unitcell  ,\
-	                        local_potental  =total     ,\
-	                        plot            =True      ,\
-	                        output_name     ='total.txt')
+    check_the_input_path(poscar)
+    check_the_input_path(locpot)
+
+
+    print '        [CODE] Start the reading 3D LOCPOT and convert to 1D'
+    unitcell, compound, position = jh.r_cryst_vasp(poscar)
+    total = read_LOCPOT_line(locpot)
+    print '        [CODE] Finish converting. Start to write'
+    write_LOCPOT_line(  unitcell        =unitcell  ,\
+	                local_potental  =total     ,\
+	                plot            =ploting   ,\
+	                output_name     ='out_locpot_1D.txt')
 
 
 if __name__ == "__main__":
-	main()
+    main()
