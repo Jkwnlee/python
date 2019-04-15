4345f36d578f0fd07c09d923fe1ca39ca6f6966b
Ji-Hwan, Lee <jkwnlee88@gmail.com>
2019-04-15 17:16:35 +0900

    Update move_poscar.py

--- py_edit_poscar/move_poscar.py
+++ py_edit_poscar/move_poscar.py
@@ -6,6 +6,8 @@
 
 import JH_lib as jh
 import os
+import numpy as np
+from optparse import OptionParser
 #1. Read.
 import sys
 
@@ -15,9 +17,23 @@ print """[CODE] run this commend with target POSCAR file name and distance which
 [CODE]              for the direction, please check after test this code.
 """
 
-## Lines for JH in SK hynix (180326)
-filename = sys.argv[1]
-unitcell, compound, position = jh.r_cryst_vasp(filename)
-new_position = jh.pst_poscar_move(position, float(sys.argv[2]), float(sys.argv[3]),float(sys.argv[4]))
-jh.w_poscar(position, compound = compound, filename = 'new.vasp', unitcell = unitcell, Selective = False)
+
+if __name__ == '__main__':
+    from time import time
+    t0 = time()
+    filename = sys.argv[1]
+    ## Lines for JH in SK hynix (180326)
+    unitcell, compound, position = jh.r_cryst_vasp(filename)
+    new_position = jh.pst_poscar_move(position, float(sys.argv[2]), \
+                                    float(sys.argv[3]),float(sys.argv[4]))
+                                    
+    if len(new_position[0]) == 7: 
+      select =  True
+    else:
+      select =  False
+      
+    jh.w_poscar(position, compound = compound, filename = 'new.vasp', unitcell = unitcell, Selective = select)
+
+    t1 = time()
+
 
