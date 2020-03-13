780cd901a579a9fc44cae1e7d0af7dec1bf0463d
Ji-Hwan, Lee <jkwnlee88@gmail.com>
2020-03-13 20:30:33 +0900

    Update merge_cell.py

--- py_edit_poscar/merge_cell.py
+++ py_edit_poscar/merge_cell.py
@@ -1,7 +1,5 @@
 #!/bin/python
-
 #Using code JH_lib
-
 #Purpose: Moving a poscar
 
 import JH_lib as jh
@@ -11,12 +9,27 @@ from optparse import OptionParser
 #1. Read.
 import sys
 ############################################################
-__version__ = "1.0"
+__version__ = "1.1"
 ############################################################
 
 def command_line_arg():
     usage = "usage: %prog [options] arg1 arg2"  
     par = OptionParser(usage=usage, version= __version__)
+
+    par.add_option("-a", '--pa', 
+            action='store',type='string', dest='file1',
+            default='./POSCARA',
+            help='Input POSCAR (Default: ./POSCARA)')
+    par.add_option("-b", '--pb',
+            action='store',type='string', dest='file2',
+            default='./POSCARB',
+            help='Input POSCAR (Default: ./POSCARB)')
+
+    par.add_option("-o", '--outname', 
+            action='store',type='string', dest='out',
+            default='./mergerd.vasp',
+            help='outputname (Default: ./merged.vasp)')
+
 
     par.add_option("-s", '--Selective', 
             action='store_true', dest='Selec',
@@ -32,9 +45,9 @@ if __name__ == '__main__':
   from time import time
   opts, args = command_line_arg()
   t0 = time()
-  filename1 = sys.argv[1]
-  filename2 = sys.argv[2]
-  output_name = filename1+'_'+filename2+'_merged.vasp'
+  filename1 = opts.file1
+  filename2 = opts.file2
+  output_name = opts.out
   ## Lines for JH in SK hynix (180326)
   unitcell1, compound1, position1 = jh.r_cryst_vasp(filename1)
   unitcell2, compound2, position2 = jh.r_cryst_vasp(filename2)
@@ -58,5 +71,3 @@ if __name__ == '__main__':
               Selective = select)
 
   t1 = time()
-
-
