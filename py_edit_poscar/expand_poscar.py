bf8d5f6c7bcb7f85c0a5ffe8170191d1f7258076
Ji-Hwan, Lee <jkwnlee88@gmail.com>
2020-03-13 20:30:14 +0900

    Update expand_poscar.py

--- py_edit_poscar/expand_poscar.py
+++ py_edit_poscar/expand_poscar.py
@@ -5,22 +5,25 @@
 #Purpose: Moving a poscar
 
 
-############################################################
-############################################################
-
 import JH_lib as jh
 import os
 import sys
 
-def expand_cell(filename,expantion,output_name):
-  print "expand the given structure  ", filename
+############################################################
+__version__ = 1.1
+############################################################
+
+def expand_cell(filename,expantion,output_name, Selec):
+  print "[CODE]              expand the given structure  ", filename
+  print """[CODE]              output: %s with selective dynamics %s
+""" %(output_name,out)
   unitcell, compound, position = jh.r_cryst_vasp(filename)
   unitcell, compound, position = jh.pst_cell_expansion(unitcell, position, expantion )
   jh.w_poscar(position = position, \
               compound = compound, \
               filename = output_name,\
               unitcell = unitcell, \
-              Selective= False)
+              Selective= Selec)
 
 
 ############################################################
@@ -31,13 +34,16 @@ if __name__ == '__main__':
 #    opts, args = command_line_arg()
   t0 = time()
   
-  filename = sys.argv[1]
+  filename = sys.argv[1];     out = "off";     Selec = False
   expantion =[int(sys.argv[2]), int(sys.argv[3]),int(sys.argv[4])]
 
-  if len(sys.argv) == 5:
+  if len(sys.argv) >= 5:
     output_name=filename+'_new.vasp'
-  elif  len(sys.argv) == 6:
+  if  len(sys.argv) >= 6:
     output_name = sys.argv[5]
+  if  len(sys.argv) >= 7:
+    Selec = True
+    out = "On"
 
 
   if  len(sys.argv) < 5:
@@ -46,11 +52,11 @@ if __name__ == '__main__':
 [CODE] for example: python /team/ptcad/jhlee/b_codework/py_edit_poscar/expand_poscar.py POSCAR 2 2 1
 [CODE]              means: expand cell in given POSCAR 2 times in x-axis and y-axis and keeping z-axis
 [CODE]              for the direction, please check after test this code.
-[CODE]              output: %s
-""" %(output_name)
+[CODE]              output: %s with selective dynamics %s
+""" %(output_name,out)
 
   else:
-    expand_cell(filename,expantion,output_name)
+    expand_cell(filename,expantion,output_name,Selec)
 
   t1 = time()
 #  print '\nCode calc completed! Time Used: %.2f [sec]\n' % (t1 - t0)
